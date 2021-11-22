f2d328ca4a22995952cafd3b84bfcd4c55f0477f
ThomasOrlita <github@thomasorlita.cz>
2021-11-22 18:15:45 +0000

    feat: send jwt from frontend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,8 +2,7 @@ import { AsyncCall, } from 'async-call-rpc';
 import { WebSocketMessageChannel } from 'async-call-rpc/utils/web/websocket.client';
 import type { Api } from '../../../server/api/Api.types';
 
-
-const server = AsyncCall<Api>(
+const serverApi = AsyncCall<Api>(
     {},
     {
         channel: new WebSocketMessageChannel('wss://' + location.hostname.replace('8000', '4000') + ':443'),
@@ -13,5 +12,16 @@ const server = AsyncCall<Api>(
         },
     },
 );
+
+// JWT middleware
+// token will be added as the last parameter for all API methods
+const server = new Proxy(serverApi, {
+    get(target, key) {
+        return new Proxy(target[key], {
+            apply: (target, _, argumentsList) =>
+                target(...argumentsList, 'jwt_will_be_here')
+        });
+    },
+});
 
 export default server;
