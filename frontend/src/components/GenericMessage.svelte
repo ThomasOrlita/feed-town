24bfd840cbbaeb279962378c96ac4a7ee6e02ef1
ThomasOrlita <github@thomasorlita.cz>
2021-11-27 19:31:10 +0000

    feat: generic message component

---
+++ frontend/src/components/GenericMessage.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Card } from 'attractions';
+</script>
+
+<div class="m-auto">
+  <Card outline class="m-4 !overflow-visible">
+    <p class="flex items-center">
+      <slot />
+    </p>
+  </Card>
+</div>
