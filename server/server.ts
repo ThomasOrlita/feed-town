1f775e8c259c1d4d5a758b609f2359765168bd4f
ThomasOrlita <github@thomasorlita.cz>
2022-03-14 08:05:02 +0000

    chore: update async-call-rpc

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,8 @@
 import * as api from './api/api.ts';
 
 import { serve } from 'https://deno.land/std@0.61.0/http/server.ts';
-import { AsyncCall } from 'https://cdn.jsdelivr.net/npm/async-call-rpc@6.0.1/out/base.mjs';
-import { WebSocketChannel } from 'https://cdn.jsdelivr.net/npm/async-call-rpc@6.0.1/utils/deno/websocket.server.ts';
+import { AsyncCall } from 'https://cdn.jsdelivr.net/npm/async-call-rpc@6.0.2/out/base.mjs';
+import { WebSocketChannel } from 'https://cdn.jsdelivr.net/npm/async-call-rpc@6.0.2/utils/deno/websocket.server.ts';
 
 
 try {
